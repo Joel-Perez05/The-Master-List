50d676c21756012e23ae6a2dc487f81cbd60fdc3
Joel Perez <joel_perez65@yahoo.com>
2022-08-20 00:59:34 -0500

    created basic header

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import { NavLink } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem} from 'reactstrap';
+
+{/* <img src="/TPHD_Master_Sword_Artwork.webp" alt="master sword picture"/> */}
+
+const Header = (props) => {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
+    return (
+        <div>
+            <Navbar className='p-5' color="success" light>
+                <NavbarBrand className="me-auto fs-1">
+                <img style={{height: "80px"}} src="/TPHD_Master_Sword_Artwork.webp" alt="master sword picture"/>
+                he Master List
+                </NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="me-2" />
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink to="/">Home</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
