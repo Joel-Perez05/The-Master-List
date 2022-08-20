7879410c453998cc7bcd8a56b676246734b684dc
Joel Perez <joel_perez65@yahoo.com>
2022-08-20 14:24:03 -0500

    finished home and details page with api integration

--- client/src/components/ZeldaHome.js
+++ client/src/components/ZeldaHome.js
@@ -6,11 +6,13 @@ import { Link } from 'react-router-dom';
 const ZeldaHome = (props) => {
     const [allGames, setAllGames] = useState([]);
     const [ocarinaOfTime, setOcarinaOfTime] = useState([]);
+    const [majorasMask, setMajorasMask] = useState([]);
+    const [breathOfTheWild, setBreathOfTheWild] = useState([]);
 
     useEffect(() => {
         axios.get("https://zelda.fanapis.com/api/games?limit=100")
             .then((res) => {
-                console.log(res.data.data);
+                // console.log(res.data.data);
                 setAllGames(res.data.data);
             })
             .catch((err) => {
@@ -21,8 +23,30 @@ const ZeldaHome = (props) => {
     useEffect(() => {
         axios.get("https://zelda.fanapis.com/api/games/5f6ce9d805615a85623ec2ba")
             .then((res) => {
-                console.log(res.data.data);
+                // console.log(res.data.data);
                 setOcarinaOfTime(res.data.data);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, []);
+
+    useEffect(() => {
+        axios.get("https://zelda.fanapis.com/api/games/5f6ce9d805615a85623ec2bc")
+            .then((res) => {
+                // console.log(res.data.data);
+                setMajorasMask(res.data.data);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, []);
+
+    useEffect(() => {
+        axios.get("https://zelda.fanapis.com/api/games/5f6ce9d805615a85623ec2c9")
+            .then((res) => {
+                // console.log(res.data.data);
+                setBreathOfTheWild(res.data.data);
             })
             .catch((err) => {
                 console.log(err)
@@ -34,7 +58,7 @@ const ZeldaHome = (props) => {
             <div className='d-flex flex-wrap mt-5'>
                 <div className='mx-auto'>
                     <Card style={{
-                        width: '18rem'
+                        width: '25rem'
                         }}>
                         <img
                             alt="Sample"
@@ -52,14 +76,59 @@ const ZeldaHome = (props) => {
                         </CardBody>
                     </Card>
                 </div>
+                <div className='mx-auto'>
+                    <Card style={{
+                        width: '25rem'
+                        }}>
+                        <img
+                            alt="Sample"
+                            src="/majoras.webp"
+                        />
+                        <CardBody>
+                            <CardTitle tag="h5">{majorasMask.name}</CardTitle>
+                            <CardText>{majorasMask.description}</CardText>
+                            <Button color='dark'>
+                                <Link style={{
+                                    color: "white",
+                                    textDecoration: "none"
+                                }} to={"/game/details/" + majorasMask.id}>Details</Link>
+                            </Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className='mx-auto'>
+                    <Card style={{
+                        width: '25rem'
+                        }}>
+                        <img
+                            alt="Sample"
+                            src="/Breath.jpg"
+                        />
+                        <CardBody>
+                            <CardTitle tag="h5">{breathOfTheWild.name}</CardTitle>
+                            <CardText>{breathOfTheWild.description}</CardText>
+                            <Button color='dark'>
+                                <Link style={{
+                                    color: "white",
+                                    textDecoration: "none"
+                                }} to={"/game/details/" + breathOfTheWild.id}>Details</Link>
+                            </Button>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
-            <div>
+            <div className='d-flex flex-wrap p-2 mt-5'>
                 {
                     allGames.map((game) => {
                         return (
-                            <div key={game.id}>
+                            <div className='mx-auto' style={{
+                                width: "15rem"
+                                }} key={game.id}>
                                 <p className='mx-auto'>
-                                    <Link to={"/game/details/" + game.id}>{game.name}</Link>
+                                    <Link style={{
+                                        textDecoration: "none",
+                                        color: "black"
+                                        }} to={"/game/details/" + game.id}>{game.name}</Link>
                                 </p>
                             </div>
                         )
