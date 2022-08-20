401276e406a28e4ab34a1446448e10d2e8ac85d7
Joel Perez <joel_perez65@yahoo.com>
2022-08-20 14:10:01 -0500

    added game details component

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,28 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import {Navigate, useParams, Link} from "react-router-dom";
+import { useParams, Link} from "react-router-dom";
+import { Card, CardBody, CardTitle, CardText, CardHeader } from 'reactstrap';
 
 const Detail = (props) => {
-    const [userLists, setUserList] = useState({})
-    const {id} = useParams(); 
-    useEffect(() => {
-        axios.get("http://localhost:8000/api/lists/" + id)
-            .then( res => {
-                console.log(res.data);
-                setPerson(res.data);
-    })
-            .catch( err => console.log(err) )
+    const [game, setGame] = useState([]);
+    const{id} = useParams(); 
 
- }, [])
+    useEffect(() => {
+        axios.get("https://zelda.fanapis.com/api/games/" + id)
+            .then( res => {
+                console.log(res.data.data);
+                setGame(res.data.data);
+    })
+            .catch( err => console.log(err) )
+    }, []);
+
     return (
-        <div>
-            <p>Username: {list.username}</p>
-            <p>First Game: {list.firstGame}</p>
-            <p>Second Game: {list.secondGame}</p>
-            <p>Third Game: {list.thirdGame}</p>
-            <p>Fourth Game: {list.fourthGame}</p>
-            <p>Fifth Game: {list.fifthGame}</p>
-            <Link to= {"/"}><button>Home</button></Link>
+        <div className='d-flex flex-wrap mt-5'>
+            <div className='mx-auto'>
+                <Card style={{
+                    width: '30rem'
+                    }}>
+                    <CardHeader tag="h2">Game Details</CardHeader>
+                    <CardBody>
+                        <CardTitle tag="h5">{game.name}</CardTitle>
+                        <CardText>{game.description}</CardText>
+                        <CardText>{game.developer}</CardText>
+                        <CardText>{game.publisher}</CardText>
+                        <CardText>{game.released_date}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     )
 }
--- client/src/components/ZeldaHome.js
+++ client/src/components/ZeldaHome.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { Button, Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const ZeldaHome = (props) => {
     const [allGames, setAllGames] = useState([]);
+    const [ocarinaOfTime, setOcarinaOfTime] = useState([]);
 
     useEffect(() => {
         axios.get("https://zelda.fanapis.com/api/games?limit=100")
@@ -14,10 +17,55 @@ const ZeldaHome = (props) => {
                 console.log(err)
             })
     }, []);
+    
+    useEffect(() => {
+        axios.get("https://zelda.fanapis.com/api/games/5f6ce9d805615a85623ec2ba")
+            .then((res) => {
+                console.log(res.data.data);
+                setOcarinaOfTime(res.data.data);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, []);
 
     return (
-        <div>
-            <h1>home</h1>
+        <div className='mx-auto'>
+            <div className='d-flex flex-wrap mt-5'>
+                <div className='mx-auto'>
+                    <Card style={{
+                        width: '18rem'
+                        }}>
+                        <img
+                            alt="Sample"
+                            src="/ocarina.jpg"
+                        />
+                        <CardBody>
+                            <CardTitle tag="h5">{ocarinaOfTime.name}</CardTitle>
+                            <CardText>{ocarinaOfTime.description}</CardText>
+                            <Button color='dark'>
+                                <Link style={{
+                                    color: "white",
+                                    textDecoration: "none"
+                                }} to={"/game/details/" + ocarinaOfTime.id}>Details</Link>
+                            </Button>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+            <div>
+                {
+                    allGames.map((game) => {
+                        return (
+                            <div key={game.id}>
+                                <p className='mx-auto'>
+                                    <Link to={"/game/details/" + game.id}>{game.name}</Link>
+                                </p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
