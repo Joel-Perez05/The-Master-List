e242027a31b7607ac0794fb0e9e4e3739c7a29ec
Joel Perez <joel_perez65@yahoo.com>
2022-11-21 18:39:00 -0600

    fixed an issue in server.js

--- server/server.js
+++ server/server.js
@@ -5,12 +5,18 @@ const PORT = 8000;
 const cors = require('cors');
 const socket = require("socket.io");
 const List = require("./models/zelda.model");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors({origin: "http://localhost:3000"}));
 // app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 
 require('./routes/zelda.routes')(app);
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const server = app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
